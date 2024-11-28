975798eb3ab5b0775552c92ca040cbc36d0c8c5b
lidonez <f15617775900@gmail.com>
2024-11-28 09:57:24 +0800

    button

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,11 @@ export const SITE = {
   author: 'Af1',
   description: '记录我的日常碎碎念，每周一发布，欢迎关注',
   keywords: 'Af1,运营，企业，营销，影视',
-  icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
+  icon: 'https://aimeezz.oss-cn-hangzhou.aliyuncs.com/Image/icon.JPG',
   pic: 'https://aimeezz.oss-cn-hangzhou.aliyuncs.com/Image/%E5%B0%81%E9%9D%A2.jpg',
   homePage: 'https://af1.fun',
   blogPage: 'https://af1.fun',
-  weiboId: '2839738845',
+  twitterId: 'aimeejjj',
   githubId: 'lidonez',
   repo: 'lidonez/weekly',
 };
