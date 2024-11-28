684b0605d0612eb54a7af9a212fdc96ad4d4a1d0
lidonez <f15617775900@gmail.com>
2024-11-28 09:57:24 +0800

    iconz

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: 'Af1',
   description: '记录我的日常碎碎念，每周一发布，欢迎关注',
   keywords: 'Af1,运营，企业，营销，影视',
-  icon: 'https://aimeezz.oss-cn-hangzhou.aliyuncs.com/Image/icon.JPG',
+  icon: 'https://aimeezz.oss-cn-hangzhou.aliyuncs.com/Image/iconz.png',
   pic: 'https://aimeezz.oss-cn-hangzhou.aliyuncs.com/Image/%E5%B0%81%E9%9D%A2.jpg',
   homePage: 'https://af1.fun',
   blogPage: 'https://af1.fun',
